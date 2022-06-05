d70a2ac69b8d059d5bce9f54afa4b54665242019
Biruk Gelelcha <byafet3@gmail.com>
2022-06-05 01:01:11 -0700

    betty proud

---
+++ betty.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ *main - is the main function
+ *return 0 - always true 
+*/
+int main() {
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
+   return 0;
+}
+
